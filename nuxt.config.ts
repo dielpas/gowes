4f936b893b46a1982fca5da68c7a6a6ec79b3eea
diel <113293505+dielpas@users.noreply.github.com>
2025-04-10 17:54:24 +0700

    add vue designer

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,10 +17,23 @@ export default defineNuxtConfig({
     '@nuxthub/core',
     '@nuxtjs/i18n',
     '@pinia/nuxt',
+    '@pinegrow/nuxt-module',
   ],
   vite: {
     plugins: [
       tailwindcss(),
     ],
+  },
+  pinegrow: {
+    liveDesigner: {
+      tailwindcss: {
+        /* Please ensure that you update the filenames and paths to accurately match those used in your project. */
+        configPath: 'tailwind.config.js',
+        cssPath: '@/assets/css/main.css',
+        // themePath: false, // Set to false so that Design Panel is not used
+        // restartOnConfigUpdate: true,
+        restartOnThemeUpdate: true,
+      }
+    },
   },
 });
